0f2227ab4611c798a0cb68080ad2855020e113ed
AnayRoshan2006 <74822497+AnayRoshan2006@users.noreply.github.com>
2021-01-20 17:41:12 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,23 @@
+class chain{
+    constructor(bodyA,bodyB){
+      var options={
+          bodyA:bodyA,
+          bodyB:bodyB,
+          stiffness:0.04,
+          length:50,
+      }
+      this.chain=Constraint.create(options)
+      World.add(world,this.chain)
+
+    }
+    display(){
+        var a=this.chain.bodyA.position
+        var b=this.chain.bodyB.position
+        strokeWeight(3)
+        line(a.x,a.y,b.x,b.y)
+
+
+
+
+    }
+}
